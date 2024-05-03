4b65653f282180987890a67dd3e4002a4fc9d4d3
Amy <amaule65@gmail.com>
2024-05-03 12:31:20 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// actual projects "my work"
+  // immobee
+  // reddit
+  // what's for dinner
+  // vanilla bean bakery
+
+// things I'm currently working on - chameleon kitchen
+
+// things I enjoy ?
+// some fun browser games I previously worked on - minesweeper, game of life, space invaders, tetris, snake
