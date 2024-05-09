f51f2537577d6b66a5adb964b2436136a9b6c374
Amy <amaule65@gmail.com>
2024-05-09 17:17:10 +0200

    Add finished and ongoing project sections

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,16 @@
 import Project from "./Project";
 
-import { projectData } from "../utilities/data";
 import { ProjectType } from "../types";
 
+type ProjectsProps = {
+  title: string,
+  projectData: ProjectType[]
+}
 
-const Projects = () => {
+const Projects = ({ title, projectData }: ProjectsProps) => {
   return (
     <section className="projects-section">
-    <h1 className="projects-title">Portfolio</h1>
+    <h1 className="projects-title">{title}</h1>
       <>
         {projectData.map((project: ProjectType) => {
           return <Project project={project} key={project.title} />
